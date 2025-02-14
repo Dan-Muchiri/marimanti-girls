798b341369799323fce4821b857e4a3108a22f0a
Dan-Muchiri <danspmunene@gmail.com>
2025-02-14 15:46:47 +0300

    Added hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-import styles from "./HeroStyles.module.css"
+import React from 'react';
+import styles from "./HeroStyles.module.css";
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <section id='hero' className={styles.container}>
+      <div className={styles.back}>
+          <div className={styles.backImage}>
+            <img src="/landscape.JPG" className={styles.image} alt="Background" />
+            <img src="/potrait.JPG" className={styles.imageSmall} alt="Small Background" />
+          </div>
+          <div className={styles.overlay} /> {/* Background overlay */}
+      </div>
+      <div className={styles.heroContent}>
+          <div className={styles.contentWrapper}>
+            <div className={styles.leftWords}>
+              <h1>Motto : Diligence for excellence</h1>
+              <div className={styles.buttons}>
+                <a href="#about">
+                      About Us
+                </a>
+                <a href="#contact" >
+                Request Info
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    </section>
   )
 }
 

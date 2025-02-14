e8a6e45281d1b0fb4c145c20a03f0c05adf76229
Dan-Muchiri <danspmunene@gmail.com>
2025-02-14 18:57:37 +0300

    Added scroll to hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,12 +2,27 @@ import React from 'react';
 import styles from "./HeroStyles.module.css";
 
 function Hero() {
+
+  const handleScroll = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+        const headerOffset = 88; // Adjust according to your header height
+        const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+        const offsetPosition = elementPosition - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth",
+        });
+    }
+};
+
   return (
     <section id='hero' className={styles.container}>
       <div className={styles.back}>
           <div className={styles.backImage}>
-            <img src="/landscape.jpg" loading="lazy" className={styles.image} alt="Background" />
-            <img src="/potrait.jpg" loading="lazy" className={styles.imageSmall} alt="Small Background" />
+            <img src="/landscape.jpg" className={styles.image} alt="Background" />
+            <img src="/potrait.jpg" className={styles.imageSmall} alt="Small Background" />
           </div>
           <div className={styles.overlay} /> {/* Background overlay */}
       </div>
@@ -16,12 +31,8 @@ function Hero() {
             <div className={styles.leftWords}>
               <h1>Motto : Diligence for excellence</h1>
               <div className={styles.buttons}>
-                <a href="#about">
-                      About Us
-                </a>
-                <a href="#contact" >
-                Request Info
-                </a>
+              <a onClick={() => handleScroll("about")}>About Us</a>
+              <a onClick={() => handleScroll("contact")}>Request Info</a>
               </div>
             </div>
           </div>

f9bcee2155c06b45f2b576305ecf288e0a5a78dd
Dan-Muchiri <danspmunene@gmail.com>
2025-02-17 21:18:05 +0300

    added react lazy loader

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import imageList from './Images'; // Importing the image URLs
 import styles from "./GalleryStyles.module.css";
+
+// Import the necessary CSS for the lazy loading
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 function Gallery() {
   return (
@@ -10,11 +14,17 @@ function Gallery() {
       </div>
       <div className={styles.galleryContainer}>
         {imageList.map((src, index) => (
-          <img key={index} src={src} alt={`Gallery image ${index + 1}`} className={styles.galleryImage} loading="lazy"/>
+          <LazyLoadImage
+            key={index}
+            src={src} // Image source
+            alt={`Gallery image ${index + 1}`} // Alt text
+            className={styles.galleryImage} // Apply your custom styling
+            effect="blur" // You can change the effect (e.g., 'opacity', 'blur', etc.)
+            loading="lazy" // Enables lazy loading with fallback in case the library fails
+          />
         ))}
       </div>
     </>
-    
   );
 }
 
